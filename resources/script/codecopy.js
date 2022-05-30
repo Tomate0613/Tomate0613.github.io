3925b60adbdb88d9ae10af9d65e79242aee4112f
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2022-05-30 18:05:51 +0200

    Add codecopy.js

---
+++ resources/script/codecopy.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded', () => document.querySelectorAll('code').forEach((element) => element.addEventListener('click', () => clientInformation.clipboard.writeText(element.innerText))));
