1e5ec8f46c5cd70e93b76c733c3a459f06ce8fcc
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2022-10-28 16:28:01 +0200

    Improve image popups

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,9 +1,9 @@
 //Background
-for(let i = 0; i < 100; i++){
+for (let i = 0; i < 100; i++) {
   const u = document.createElement('div');
   u.classList.add('box');
   u.style.animationDelay = `${Math.random() * -100}s`;
-  u.style.animationDuration = `${Math.random() * 15 + 15}s`
+  u.style.animationDuration = `${Math.random() * 15 + 15}s`;
   u.style.left = `${i}vw`;
 
   document.getElementById('container').appendChild(u);
@@ -11,7 +11,7 @@ for(let i = 0; i < 100; i++){
 
 const projects = document.getElementsByClassName('project');
 
-for(let i = 0; i < projects.length; i++){
+for (let i = 0; i < projects.length; i++) {
   const item = projects.item(i);
   item.addEventListener('click', () => {
     window.location.href = item.getAttribute('href');
