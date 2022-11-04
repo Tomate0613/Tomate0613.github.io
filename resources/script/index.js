d8485ed9fae2ee7e682bfcedb37d4f21bc135102
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2022-11-04 12:07:49 +0100

    edit tomate-launcher and msc page

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,31 +1,29 @@
 //Background
 for (let i = 0; i < 100; i++) {
-  const u = document.createElement('div');
-  u.classList.add('box');
+  const u = document.createElement("div");
+  u.classList.add("box");
   u.style.animationDelay = `${Math.random() * -100}s`;
   u.style.animationDuration = `${Math.random() * 15 + 15}s`;
   u.style.left = `${i}vw`;
 
-  document.getElementById('container').appendChild(u);
+  document.getElementById("container").appendChild(u);
 }
 
-const projects = document.getElementsByClassName('project');
+//Projects
+const projects = document.getElementsByClassName("project");
 
-for (let i = 0; i < projects.length; i++) {
-  const item = projects.item(i);
-  item.addEventListener('click', () => {
-    window.location.href = item.getAttribute('href');
-  });
+const randomImage = (folder, start, stop) => {
+  return `${folder}/${
+    Math.floor(Math.random() * (stop - start + 1)) + start
+  }.png`;
+}
 
-  const folder = item.dataset.folder;
-  const start = parseInt(item.dataset.start);
-  const stop = parseInt(item.dataset.stop);
+document.querySelectorAll(".project").forEach((project) => {
+  const folder = project.dataset.folder;
+  const start = parseInt(project.dataset.start);
+  const stop = parseInt(project.dataset.stop);
 
-  const image = item.getElementsByClassName('image')[0];
-  setInterval(() => image.src = randomImage(folder, start, stop), 5000);
+  const image = project.getElementsByClassName("image")[0];
+  setInterval(() => (image.src = randomImage(folder, start, stop)), 5000);
   image.src = randomImage(folder, start, stop);
-}
-
-function randomImage(folder, start, stop) {
-  return `${folder}/${Math.floor(Math.random() * (stop - start + 1)) + start}.png`;
-}
+});
