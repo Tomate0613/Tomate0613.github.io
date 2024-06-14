5faa67c769baad580e7284aa5c5ca2cc6d3ea165
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-06-14 17:01:42 +0200

    Fix and refactor some stuff

--- resources/script/image-cycle.js
+++ resources/script/image-cycle.js
@@ -5,19 +5,22 @@ function imageSrc(folder, idx, fileType) {
 const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t));
 
 async function setupImages() {
-  console.log("letse go");
-  const images = document.querySelectorAll(".img-cycle");
+  const images = document.querySelectorAll('.img-cycle');
 
   for (let i = 0; i < images.length; i++) {
     const elem = images[i];
     console.log(elem);
 
-    const folder = elem.dataset["folder"];
-    const times = elem.dataset["times"]?.split(",").map((n) => +n);
-    const start = +elem.dataset["start"];
-    const stop = +(elem.dataset["stop"] ?? times?.length);
-    const filetype = elem.dataset["filetype"];
-    const random = elem.dataset["random"];
+    const folder = elem.dataset['folder'];
+    const filetype = elem.dataset['filetype'];
+    const times = elem.dataset['times']?.split(',').map((n) => +n);
+    const start = +(
+      elem.dataset['start'] ??
+      elem.attributes.src.nodeValue.replace(`${folder}/`, '').replace(`.${filetype}`, '')
+    );
+    console.log(start);
+    const stop = +(elem.dataset['stop'] ?? times?.length);
+    const random = elem.dataset['random'];
 
     let idx = start;
 
@@ -27,7 +30,7 @@ async function setupImages() {
       await sleep(times?.[idx - start] ?? 12000);
 
       if (!random && ++idx > stop) idx = start;
-      if(random) idx = Math.round(Math.random() * (stop - start)) + start;
+      if (random) idx = Math.round(Math.random() * (stop - start)) + start;
     }
   }
 }
--- resources/script/index.js
+++ resources/script/index.js
@@ -17,24 +17,3 @@ const imageSrc = (folder, idx, fileType) => {
 };
 
 const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t));
-
-document.querySelectorAll('.image-cycle').forEach(async (project) => {
-  const folder = project.dataset.folder;
-  const start = parseInt(project.dataset.start);
-  const stop = parseInt(project.dataset.stop);
-  const fileType = project.dataset.filetype ?? 'png';
-  const times = project.dataset.times?.split(',');
-
-  const image = project.getElementsByClassName('image')[0];
-
-  image.src = imageSrc(folder, 0, fileType);
-
-  let idx = start - 1 + 2;
-
-  while (true) {
-    if (++idx > stop) idx = start;
-
-    image.src = imageSrc(folder, idx, fileType);
-    await sleep(times?.[idx - start] ?? 12000);
-  }
-});
